3d11e3ecafe21216a4f14569a1f7ad3dc3c90509
Obseen Costington <winsight@mail.ru>
2026-02-13 12:12:51 +0700

    Валидатор юзера и тесты

--- groupone/src/main/java/groupone/model/UserABC.java
+++ groupone/src/main/java/groupone/model/UserABC.java
@@ -10,6 +10,20 @@ public abstract class UserABC {
     public int getPassword() { return password; }
     public String getEmail() { return email; }
 
+    private UserABC() {}
+    public UserABC(String username, int password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public boolean isValid() {
+        if (username == null || email == null) { return false; }
+        return (username.matches("^([a-zA-Zа-яА-Я]+([-\\s][a-zA-Zа-яА-Я]+)*)$")
+                && password >= 0
+                && email.matches("^([a-zA-Z0-9]+(\\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+\\.[a-zA-Z]+)$"));
+    }
+
     @Override
     public String toString() {
         return "Имя: " + username + ", Пароль: " + password + ", Почта: " + email;
--- groupone/src/test/java/groupone/model/TesterTest.java
+++ groupone/src/test/java/groupone/model/TesterTest.java
@@ -9,9 +9,53 @@ import static org.junit.jupiter.api.Assertions.*;
 class TesterTest {
 
     @Test
-    void testBuild() {
-        DataInterface di = new RandomGenerator(5);
-        di.forEach(System.out::println);
+    void testBuilder() {
+        User user = new User.Builder()
+                .setUsername(" Иван Иванович ")
+                .setPassword(123)
+                .setEmail("ivan.ivan@mail.ru ")
+                .build();
+        assertTrue(user.isValid());
+        assertEquals("Иван Иванович", user.getUsername());
+        assertEquals(123, user.getPassword());
+        assertEquals("ivan.ivan@mail.ru", user.getEmail());
+        user = new User.Builder()
+                .setLine("  Aula fon der Bruman,  22884,valld.emar@gmail.com")
+                .build();
+        assertTrue(user.isValid());
+        assertEquals("Aula fon der Bruman", user.getUsername());
+        assertEquals(22884, user.getPassword());
+        assertEquals("valld.emar@gmail.com", user.getEmail());
+        user = new User.Builder()
+                .setLine("22884,Aula fon der Bruman,valld.emar@gmail.com")
+                .build();
+        assertFalse(user.isValid());
+        user = new User.Builder()
+                .setLine("Aula fon der Bruman,valld.emar@gmail.com")
+                .build();
+        assertFalse(user.isValid());
+        user = new User.Builder()
+                .setLine("Aula fon der Bruman,22884,valld.emar@gmail.com, ")
+                .build();
+        assertFalse(user.isValid());
+        user = new User.Builder()
+                .setLine("Aula fon der Bruman,22884,valld.emar@gmailcom")
+                .build();
+        assertFalse(user.isValid());
+        user = new User.Builder()
+                .setLine("Aula fon der Bruman,22884,valldemar@gmail.com")
+                .build();
+        assertTrue(user.isValid());
+        user = new User.Builder()
+                .setLine("Aula fon der Bruman,,valld.emar@gmail.com")
+                .build();
+        assertFalse(user.isValid());
+    }
+
+    @Test
+    void testDI() {
+        DataInterface di = new RandomGenerator(50);
+        di.forEach(item -> assertTrue(new User.Builder().setLine(item).build().isValid()));
     }
 
 }
