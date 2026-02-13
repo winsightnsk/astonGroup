7b5d6a6201019516c0688b5d8a7260a34f6c0e43
Obseen Costington <winsight@mail.ru>
2026-02-13 23:44:27 +0700

    info

--- groupone/src/main/java/groupone/model/UserABC.java
+++ groupone/src/main/java/groupone/model/UserABC.java
@@ -17,6 +17,14 @@ public abstract class UserABC {
         this.email = email;
     }
 
+    /**
+     * Проверка заполнения полей на валидность данных
+     * Имя пользователя: "^([a-zA-Zа-яА-Я]+([-\\s][a-zA-Zа-яА-Я]+)*)$"
+     * одно или больше слов через один пробел или дефис;
+     * пароль: число больше ноля;
+     * Е-мэйл: поддерживает точки в имени (не повторяющиеся)
+     * @return True если всё хорошо
+     */
     public boolean isValid() {
         if (username == null || email == null) { return false; }
         return (username.matches("^([a-zA-Zа-яА-Я]+([-\\s][a-zA-Zа-яА-Я]+)*)$")
