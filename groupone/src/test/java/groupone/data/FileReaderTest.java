91419541b3418857b90e53e1cf5b206dc9a1b7e6
W1ntrest <vlad.ermalinsky@gmail.com>
2026-02-14 15:00:43 +0300

    FileReader

--- groupone/src/test/java/groupone/data/FileReaderTest.java
+++ groupone/src/test/java/groupone/data/FileReaderTest.java
@@ -2,12 +2,19 @@ package groupone.data;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FileReaderTest {
 
     @Test
     void Test(){
-
+        try(FileReader reader = new FileReader(1, "resources/users.txt")){
+            for(String line : reader)
+                System.out.println(line);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
