50e5ea18ac53339cbbbecd02763c1aec445b35c3
Ilya Volkov <involkov2022@gmail.com>
2026-02-15 12:40:41 +0300

    Вторая доработка теста к дополнительному заданию 1.

--- groupone/src/test/java/groupone/domain/SortAltTest.java
+++ groupone/src/test/java/groupone/domain/SortAltTest.java
@@ -6,32 +6,50 @@ import groupone.model.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class SortAltTest {
-    public static boolean isEvenSorted(List<User> list, SortInterface context) {
-        // Выделить пользователей с чётным паролем
-        List<User> evenUsers = new LinkedList<>();
-        for (User user : list) {
-            if (user.getPassword() % 2 == 0)
-                evenUsers.add(user);
+    public static List<Integer> odds(List<User> list) {
+        // Сохраняем в список индексы нечётных элементов
+        List<Integer> odds = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getPassword() % 2 != 0)
+                odds.add(i);
         }
 
-        // Сохраняем копию "чётного" списка
-        List<User> evenCopy = new LinkedList<>(evenUsers);
-        context.sort(evenCopy);
+        return odds;
+    }
 
-        // Проверяем совпадение позиций чётных объектов
-        int expectedIndex = 0;
-        for (User user : list) {
-            if (user.getPassword() % 2 == 0) {
-                // Проверяем, совпадает ли реальный чётный объект с ожидаемым отсортированным
-                if (evenCopy.get(expectedIndex).getPassword() != user.getPassword()) {
-                    return false;
-                }
-                expectedIndex++;
+    public static boolean isEvenSorted(List<User> list, List<Integer> odds) {
+        /* Проверяем, что отсортированы только чётные элементы:
+         * смотрим на пару с индексами i, j. Если i-й элемент нечётный, то двигаемся до ближайшего чётного.
+         * Сравниваем с j-м элементом, начиная с j = i + 1.
+         * Если j-й элемент нечётный, то пропускаем его
+         */
+        for (int i = 0; i < list.size() - 1; i++){
+            if (list.get(i).getPassword() % 2 != 0) {
+                continue;
             }
+
+            int j = i + 1;
+
+            while (j < list.size() && list.get(j).getPassword() % 2 != 0) {
+                j++;
+            }
+
+            if (j >= list.size())
+                break;
+
+            if (list.get(i).getPassword() > list.get(j).getPassword())
+                return false;
+        }
+
+        // Проверяем, что нечётные элементы сохранили свои позиции
+        for (Integer index : odds) {
+            if (list.get(index).getPassword() % 2 == 0)
+                return false;
         }
 
         return true;
@@ -43,10 +61,12 @@ public class SortAltTest {
         DataInterface di = new RandomGenerator(50);
         di.forEach(item -> users.add(new User.Builder().setLine(item).build()));
 
+        List<Integer> odds = odds(users);
+
         SortInterface context = new SortContext();
         context.setSortStrategy(new SortByPasswordAltStrategy());
         context.sort(users);
 
-        Assertions.assertTrue(isEvenSorted(users, context));
+        Assertions.assertTrue(isEvenSorted(users, odds));
     }
 }
